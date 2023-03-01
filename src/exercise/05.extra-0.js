55c1b07c2ab4f2833f52f4ffa8ca09c37a873292
Christoph Planken <c.planken@absturzsicherung.de>
2023-03-01 10:25:23 +0100

    finished 05 with all extras

--- src/exercise/05.extra-0.js
+++ src/exercise/05.extra-0.js
@@ -6,14 +6,10 @@ import {
   fetchPokemon,
   PokemonInfoFallback,
   PokemonForm,
+  PokemonDataView,
   PokemonErrorBoundary,
-  getImageUrlForPokemon,
 } from '../pokemon'
 import {createResource} from '../utils'
-
-const PokemonInfo = React.lazy(() =>
-  import('../lazy/pokemon-info-render-as-you-fetch'),
-)
 
 // ❗❗❗❗
 // 🦉 On this one, make sure that you UNCHECK the "Disable cache" checkbox
@@ -27,6 +23,29 @@ function preloadImage(src) {
     img.src = src
     img.onload = () => resolve(src)
   })
+}
+
+const imgSrcResourceCache = {}
+
+function Img({src, alt, ...props}) {
+  let imgSrcResource = imgSrcResourceCache[src]
+  if (!imgSrcResource) {
+    imgSrcResource = createResource(preloadImage(src))
+    imgSrcResourceCache[src] = imgSrcResource
+  }
+  return <img src={imgSrcResource.read()} alt={alt} {...props} />
+}
+
+function PokemonInfo({pokemonResource}) {
+  const pokemon = pokemonResource.read()
+  return (
+    <div>
+      <div className="pokemon-info__img-wrapper">
+        <Img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <PokemonDataView pokemon={pokemon} />
+    </div>
+  )
 }
 
 const SUSPENSE_CONFIG = {
@@ -48,9 +67,7 @@ function getPokemonResource(name) {
 }
 
 function createPokemonResource(pokemonName) {
-  const data = createResource(fetchPokemon(pokemonName))
-  const image = createResource(preloadImage(getImageUrlForPokemon(pokemonName)))
-  return {data, image}
+  return createResource(fetchPokemon(pokemonName))
 }
 
 function App() {
